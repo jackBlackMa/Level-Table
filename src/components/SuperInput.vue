16db9234652c7f24469e3fc02f65cda5e27b261e
madarong <1542265532@qq.com>
2020-06-10 23:43:17 +0800

    input

---
+++ src/components/SuperInput.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-input v-model="input" placeholder="请输入内容" @input.native="handleChange"></el-input>
+</template>
+
+<script>
+    export default {
+        name: "SuperInput",
+      data() {
+          return {
+            input: ''
+          }
+      },
+      methods: {
+        handleChange(data) {
+          this.input= data.target.value.replace(/[^\d.]/g, '')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/Test'
+import SuperInput from '@/components/SuperInput'
 
 Vue.use(Router)
 
@@ -10,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'Test',
-      component: Test
+      component: SuperInput
     }
   ]
 })
