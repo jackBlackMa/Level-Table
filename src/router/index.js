2f26c0ed99d08de75a652de17a5de0b1f5824874
madarong <1542265532@qq.com>
2020-07-05 22:48:28 +0800

    feat(emmet): quickLayout

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Test from '@/components/Test'
 import SuperInput from '@/components/SuperInput'
 import Select from '@/components/Select'
 import Collapse from '@/views/collapse'
+import quickLayout from '@/views/quickLayout'
 
 Vue.use(Router)
 
@@ -14,6 +15,11 @@ export default new Router({
       path: '/',
       name: 'Test',
       component: Collapse
+    },
+    {
+      path: '/emmet',
+      name: 'emmet',
+      component: quickLayout
     }
   ]
 })
---
+++ src/views/quickLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="6">1123</el-col>
+      <el-col :span="6">2123</el-col>
+      <el-col :span="6">3123</el-col>
+      <el-col :span="6">4123</el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">1你好</el-col>
+      <el-col :span="4">2你好</el-col>
+      <el-col :span="4">3你好</el-col>
+      <el-col :span="4">4你好</el-col>
+      <el-col :span="4">5你好</el-col>
+      <el-col :span="4">6你好</el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">1你好</el-col>
+      <el-col :span="4">2你好</el-col>
+      <el-col :span="4">3你好</el-col>
+      <el-col :span="4">4你好</el-col>
+      <el-col :span="4">5你好</el-col>
+      <el-col :span="4">6你好</el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">1你好</el-col>
+      <el-col :span="4">2你好</el-col>
+      <el-col :span="4">3你好</el-col>
+      <el-col :span="4">4你好</el-col>
+      <el-col :span="4">5你好</el-col>
+      <el-col :span="4">6你好</el-col>
+    </el-row>
+    <el-row>
+      <el-col span="6" align="center">1</el-col>
+      <el-col span="6" align="center">2</el-col>
+      <el-col span="6" align="center">3</el-col>
+      <el-col span="6" align="center">4</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "quickLayout"
+  }
+</script>
+
+<style scoped>
+
+</style>
